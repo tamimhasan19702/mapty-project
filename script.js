e1eb7876ba240957a0f01e851cfb7ffdf866e4ba
tamimhasan19702 <tamimhasancu@gmail.com>
2022-07-24 22:48:05 +0600

    refactor js - part - 1

--- script.js
+++ script.js
@@ -15,37 +15,87 @@ let map,mapEvent;
 
 // ===========================================================
 
-if(navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(function(Position){
-    const {latitude} = Position.coords;
-    const {longitude} = Position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+class App{
+ 
+  #map;
+  #mapEvent;
 
-   const coords = [latitude,longitude]
+  constructor(){
+    this._getPosition(); 
+  }
+  _getPosition(){
 
-   map = L.map('map').setView(coords,15);
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(this._loadMap ,function(){
+        alert(`Couldn't get your position!!`)
+      })
+    }
+    
+  }
 
-   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-map.on('click', (mapE) => {
-  mapEvent = mapE
- form.classList.remove('hidden');
- inputDistance.focus();
-
-})
-
-
-
-  },function(){
-    alert(`Couldn't get your position!!`)
+  _loadMap(Position){   
+      const {latitude} = Position.coords;
+      const {longitude} = Position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  
+     const coords = [latitude,longitude]
+  
+     map = L.map('map').setView(coords,16);
+  
+     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+  
+  
+  map.on('click', (mapE) => {
+    mapEvent = mapE
+   form.classList.remove('hidden');
+   inputDistance.focus();
+  
   })
+ 
+  }
+
+  _showForm(){
+
+  }
+
+  _toggleElevationField(){
+
+  }
+
+  _newWorkout(){
+
+  }
+
 }
+
+const app = new App();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ================================================================
 
 
 // ----------------------------------------------------------
+
+
+
 
 form.addEventListener('submit' , (e) => {
 
